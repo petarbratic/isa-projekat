213cbbbddbe5f586d902e9955272f806e9806dc0
Petar Bratić <bratic.petar03@gmail.com>
2025-12-21 16:28:30 +0100

    basic test to see if back is working

---
+++ isa-backend/src/main/java/rs/ac/ftn/isa/backend/api/PingController.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.isa.backend.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/api/ping")
+    public String ping() {
+        return "pong";
+    }
+}
