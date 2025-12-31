4f6b5ad07b3a0d18ae27803db6a6d779ad1cca0f
Petar Bratić <bratic.petar03@gmail.com>
2025-12-31 16:52:54 +0100

    prepisano iz vezbi sign up na frontu

--- isa-frontend/src/app/app-routing.module.ts
+++ isa-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
--- isa-frontend/src/app/app.module.ts
+++ isa-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
