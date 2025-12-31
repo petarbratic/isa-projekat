9dc6569f28eea9d9ecadb6ee161959acc0ab0d44
Petar Bratić <bratic.petar03@gmail.com>
2025-12-31 17:12:08 +0100

    prepisan login

--- isa-frontend/src/app/features/auth/login/login.component.ts
+++ isa-frontend/src/app/features/auth/login/login.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { UserService } from 'src/app/core/services/user.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+
+
+interface DisplayMessage {
+  msgType: string;
+  msgBody: string;
+}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  title = 'Login';
+  form!: FormGroup;
+
+  /**
+   * Boolean used in telling the UI
+   * that the form has been submitted
+   * and is awaiting a response
+   */
+  submitted = false;
+
+   /**
+   * Notification message from received
+   * form request or router
+   */
+    notification!: DisplayMessage;
+
+    returnUrl!: string;
+    private ngUnsubscribe: Subject<void> = new Subject<void>();
+
+
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.route.params
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((params: any) => {
+        this.notification = params as DisplayMessage;
+      });
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.form = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(64)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(32)])]
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  onSubmit() {
+    /**
+     * Innocent until proven guilty
+     */
+    
+    this.notification;
+    this.submitted = true;
+
+    this.authService.login(this.form.value)
+      .subscribe(data => {
+        console.log(data);
+          this.userService.getMyInfo().subscribe();
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          console.log(error);
+          this.submitted = false;
+          this.notification = {msgType: 'error', msgBody: 'Incorrect username or password.'};
+        });
+  }
 
 }
