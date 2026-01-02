43a99477b04fbf6bcbea83b2905796fda8372a0f
Petar Bratić <bratic.petar03@gmail.com>
2026-01-02 12:52:58 +0100

    minor fix skripta

--- isa-backend/src/main/resources/import.sql
+++ isa-backend/src/main/resources/import.sql
@@ -2,11 +2,9 @@
 -- Lozinka za oba user-a je 123
 
 INSERT INTO USERS (username, password, first_name, last_name, email, address, enabled, last_password_reset_date) VALUES ('user', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Marko', 'Markovic', 'user@example.com', 'adresa1', true, '2017-10-01 21:58:58.508-07');
-INSERT INTO USERS (username, password, first_name, last_name, email, address, enabled, last_password_reset_date) VALUES ('admin', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Nikola', 'Nikolic', 'admin@example.com', 'adresa2', true, '2017-10-01 18:57:58.508-07');
+INSERT INTO USERS (username, password, first_name, last_name, email, address, enabled, last_password_reset_date) VALUES ('user2', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Nikola', 'Nikolic', 'user2@example.com', 'adresa2', true, '2017-10-01 18:57:58.508-07');
 
 INSERT INTO ROLE (name) VALUES ('ROLE_USER');
-INSERT INTO ROLE (name) VALUES ('ROLE_ADMIN');
-INSERT INTO ROLE (name) VALUES ('ROLE_ZEC');
 
 INSERT INTO USER_ROLE (user_id, role_id) VALUES (1, 1); -- user-u dodeljujemo rolu USER
 INSERT INTO USER_ROLE (user_id, role_id) VALUES (2, 1); -- admin-u dodeljujemo rolu USER
